330a6d734d081a6d0954bbe3f30fdf143bebf190
Vladimir Levin <vladimirlevinozinki@gmail.com>
2021-07-03 21:39:58 +0300

    Preparation

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "CLI/App.hpp"
+#include "CLI/Formatter.hpp"
+#include "CLI/Config.hpp"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout << "Hi there!" << endl;
+
+    return 0;
+}
