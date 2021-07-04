59fa8ad1bfb325ad7d835208c4eab2828e1b1ea5
Vladimir Levin <vladimirlevinozinki@gmail.com>
2021-07-04 12:11:43 +0300

    Templater module

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,19 @@
 #include "CLI/Formatter.hpp"
 #include "CLI/Config.hpp"
 
+#include "subcommands/templater.cpp"
+
 using namespace std;
 
 int main(int argc, char **argv)
 {
-    cout << "Hi there!" << endl;
+    CLI::App app{"Universal package"};
+    app.allow_extras(true);
+    app.require_subcommand(1);
+
+    Templater::init(app);
+
+    CLI11_PARSE(app, argc, argv);
 
     return 0;
 }
---
+++ src/utils/string.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+namespace Utils
+{
+    bool replace(string &str, const string &from, const string &to)
+    {
+        size_t start_pos = str.find(from);
+        if (start_pos == string::npos)
+            return false;
+        str.replace(start_pos, from.length(), to);
+        return true;
+    }
+
+    void replace_all(string &str, const string &from, const string &to)
+    {
+        bool success = Utils::replace(str, from, to);
+
+        while (success)
+        {
+            success = Utils::replace(str, from, to);
+        }
+    }
+}
